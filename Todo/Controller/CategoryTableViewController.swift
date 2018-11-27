0e002d4ff1c8a05783099308d3769793085ee11e
Animesh Mishra <anios49@icloud.com>
2018-11-27 11:32:38 +0530

    Added Category VC.

---
+++ Todo/Controller/CategoryTableViewController.swift
@@ -0,0 +1,110 @@
+//
+//  CategoryTableViewController.swift
+//  Todo
+//
+//  Created by Animesh Mishra on 26/11/18.
+//  Copyright © 2018 Animesh Mishra. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryTableViewController: UITableViewController {
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var itemArray = [Category]()
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        loadData()
+    }
+
+  
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        cell.textLabel!.text = itemArray[indexPath.row].name
+        
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "CategorySegue", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "New Category", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Category", style: .default) { (action) in
+            if (!(textField.text?.isEmpty)!) {
+                if let data = textField.text {
+                    let newItem = Category(context: self.context)
+                    newItem.name = data
+                    self.itemArray.append(newItem)
+                }
+                self.saveData()
+            }
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Enter Category"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    //MARK: - Data Manipulation Methoeds
+    
+    func loadData(with request:NSFetchRequest<Category> = Category.fetchRequest()) {
+        do {
+            itemArray = try context.fetch(request)
+        }
+        catch {
+            print("Error in fetch")
+        }
+        tableView.reloadData()
+    }
+    
+    func saveData() {
+        do {
+            try context.save()
+        }
+        catch {
+            print("Error in Save")
+        }
+        tableView.reloadData()
+    }
+}
+
+
+extension CategoryTableViewController:UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request:NSFetchRequest<Category> = Category.fetchRequest()
+        request.predicate = NSPredicate(format: "name CONTAINS[cd] %@", searchBar.text!)
+        request.sortDescriptors = [NSSortDescriptor(key: "name", ascending: true)]
+        loadData(with: request)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if (searchBar.text?.count == 0) {
+            loadData()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+        }
+    }
+}
